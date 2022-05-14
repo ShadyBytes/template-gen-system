41f4810a6a3fd91a672990fd14ff042c523a243f
Timur <10380147+ShadyBytes@users.noreply.github.com>
2022-05-14 17:00:19 +0300

    init

---
+++ src/test/java/com/timur/templategensystem/TemplateGenSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timur.templategensystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateGenSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
