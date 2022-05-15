02f829d7062fa3f29319536feca98fb5aef34298
Timur <10380147+ShadyBytes@users.noreply.github.com>
2022-05-15 23:57:35 +0300

    enhancments

---
+++ src/main/java/com/timur/templategensystem/Controllers.java
@@ -0,0 +1,46 @@
+package com.timur.templategensystem;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+@Controller
+public class Controllers {
+    @GetMapping("/healthcheck")
+    String healthcheck(Model model) {
+//        return System.getProperty("user.dir");
+        model.addAttribute("directory", System.getProperty("user.dir"));
+        return "healthcheck";
+    }
+
+    @GetMapping("/logs")
+    String getFile(@RequestParam String filename, Model model) {
+        Path filePath = Path.of(System.getProperty("user.dir") + "\\" + filename);//"logs.txt");
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream
+                     = Files.lines(Paths.get(String.valueOf(filePath)), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s).append("\n"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String fileContent = contentBuilder.toString();
+
+        model.addAttribute("log", fileContent);
+        return "logs";
+    }
+
+    @PostMapping("/json")
+    @ResponseBody
+    String json(@RequestBody ShopItem ShopItem) {
+        return ShopItem.getName() + " " + ShopItem.getId() + " " + ShopItem.getPrice();
+    }
+}
